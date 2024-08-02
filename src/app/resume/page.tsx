6d859fddbf5b22f050f488e762b9245c44b28c24
WANG Jiatao <129773676+TavinWang@users.noreply.github.com>
2024-08-02 13:46:50 +0800

    Update page.tsx

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
             <Dot className='cursor-not-allowed bg-green-500' />
           </span>
           <span className='text-center text-gray-400'>
-            zhangyu@MacBook-Air:~
+            About Me
           </span>
           <span className='text-end text-gray-500'>⌥⌘1</span>
         </header>
@@ -57,20 +57,19 @@ export default function Page() {
             <TypedText>whoami</TypedText>
             <TypedContent>
               <p>
-                Hi, I&apos;m <strong>ZHANG YU</strong>, in Chinese my name is{' '}
-                <strong>张宇</strong>.
+                Hi, I&apos;m <strong>WANG Jiatao</strong>, in Chinese my name is{' '}
+                <strong>王嘉涛</strong>.
               </p>
               <p>
-                I am a senior front-end engineer who have been involving in
-                front-end development since <strong>2018</strong>, focusing on
-                building aesthetically pleasing and highly interactive user
-                interfaces by <code>React</code>.
+                I am a undergraduate student majoring in <strong>Data Science and Business Analytics</strong>
+                at Central South University, focusing on
+                focusing on the frontiers of machine learning and big data by <code>Python</code>.
               </p>
               <p>
                 I have amassed substantial practical experience in the{' '}
-                <code>React</code> technology stack.
+                <strong>DS</strong>.
               </p>
-            </TypedContent>
+            {/* </TypedContent>
             <TypedText afterDelay={1000}>ls</TypedText>
             <TypedContent>
               <div className='grid grid-cols-2 gap-2 px-4 font-semibold text-sky-500'>
@@ -81,8 +80,8 @@ export default function Page() {
                 <span>skills</span>
                 <span>contact</span>
               </div>
-            </TypedContent>
-            <TypedText afterDelay={700}>opensource</TypedText>
+            </TypedContent> */}
+            {/* <TypedText afterDelay={700}>opensource</TypedText>
             <TypedContent>
               <p>
                 I am passionate about contributing to the open-source community,
@@ -94,20 +93,20 @@ export default function Page() {
                 open-source projects, which have been successfully published on
                 npm and collectively garnered nearly <i>20,000</i> downloads.
               </p>
-            </TypedContent>
+            </TypedContent> */}
             <TypedText afterDelay={1000}>projects</TypedText>
             <TypedContent>
               <ul>
                 <li>
                   <strong>
-                    <a href='https://github.com/zhangyu1818/react-markdown-toc'>
+                    <a href='https://github.com/TavinWang/Machine-Learning'>
                       react-markdown-toc
                     </a>
                   </strong>
                 </li>
-                <li>Generating a Table of Contents (TOC) from Markdown.</li>
+                <li>Machine learning in action.</li>
               </ul>
-              <ul>
+              {/* <ul>
                 <li>
                   <strong>
                     <a href='https://github.com/zhangyu1818/from-to'>
@@ -155,57 +154,44 @@ export default function Page() {
                   Effortless FLIP animations with a React Hook for smoother
                   transitions.
                 </li>
-              </ul>
+              </ul> */}
             </TypedContent>
             <TypedText>blog-info</TypedText>
             <TypedContent>
               <p>
                 I am keen on sharing knowledge as well as writing articles and
-                tutorials regularly, covering <code>JavaScript</code>,
-                <code>TypeScript</code>, <code>React</code> source-code,{' '}
-                <code>CSS</code>, and animation.
+                tutorials regularly, covering <code>Python</code>,
+                <code>Java</code>, <code>MATLAB</code>,
+                and <code>Sql</code>.
               </p>
               <p>
-                My articles have been read over <i>150,000</i> times.
+                {/* My articles have been read over <i>150,000</i> times. */}
                 Previously, I primarily wrote in Chinese, but currently, my goal
                 is to write articles in English.
               </p>
             </TypedContent>
             <TypedText>experience</TypedText>
             <TypedContent>
-              <p>Until now, I have worked for two companies only.</p>
+              <p><i>2021-2025</i><strong>Central South University</strong></p>
               <p>
-                The first company focused on developing high-performance and
-                versatile dialing systems and management platforms for internal
-                enterprise use, along with various mini-programs and Apps.
-              </p>
-              <p>
-                The second company specialized in creating visually appealing
-                and high-performance e-commerce platforms and Apps for
-                cross-border trade.
+                I studied in the Business School of Central South University as an undergraduate, 
+                majoring in information management and information systems, 
+                and my research direction is data science and business analysis
               </p>
             </TypedContent>
             <TypedText>skills</TypedText>
             <TypedContent>
               <p>
-                I excel in <code>JavaScript</code> and <code>TypeScript</code>,
-                proficiently utilizing <code>CSS</code> to create visually
-                appealing and responsive web pages.
+                I excel in <code>Python</code> and <code>MATLAB</code>,
+                proficiently utilizing <code>Sql</code> for various data operations.
               </p>
               <p>
-                I have a deep understanding of <code>React</code> and have
-                explored other popular frameworks as well. Currently, my work
-                primarily involves using <code>Next.js</code>, and I have
-                extensive experience with <code>App Router</code>.
+                In terms of data analysis, I am proficient in <code>Excel</code> and <code>Tableau</code>, 
+                using them for data visualization and descriptive statistics
               </p>
               <p>
-                In addition to front-end development, I am also familiar with
-                backend development using tools such as <code>Prisma</code>,{' '}
-                <code>NestJS</code>, and <code>GraphQL</code>.
-              </p>
-              <p>
-                Furthermore, I have experience in mobile App development using
-                <code>React Native</code> and <code>SwiftUI</code>.
+                In addition to machine learning, I am also familiar with
+                algorithm such as decision trees, SVM, neural networks, and logistic regression.
               </p>
             </TypedContent>
             <TypedText>Contact</TypedText>
@@ -213,16 +199,14 @@ export default function Page() {
               <div className='my-4 flex items-center'>
                 <p className='basis-1/4 text-center font-semibold'>Social</p>
                 <div className='grid flex-1 grid-cols-2 justify-items-start gap-2'>
-                  <a href='https://github.com/zhangyu1818'>Github</a>
-                  <a href='https://twitter.com/zhangyu1818'>Twitter</a>
-                  <a href='https://juejin.cn/user/4089838986339927'>掘金</a>
-                  <a href='https://dev.to/zhangyu1818'>Dev.to</a>
-                  <a href='https://medium.com/@zhangyu1818'>Medium</a>
+                  <a href='https://github.com/TavinWang'>Github</a>
+                  <a href='https://juejin.cn/user/2203888091411038'>掘金</a>
+                  <a href='https://medium.com/@tavin.wang'>Medium</a>
                 </div>
               </div>
               <div className='flex items-center'>
                 <p className='basis-1/4 text-center font-semibold'>Email</p>
-                <a href='mailto:hey@zhangyu.dev'>hey@zhangyu.dev</a>
+                <a href='mailto:wjt@163.com'>wjt@163.com</a>
               </div>
             </TypedContent>
           </Typed>
