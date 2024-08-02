8c7e08b1d3323caa87f846cc2da6f565bded36e7
WANG Jiatao <129773676+TavinWang@users.noreply.github.com>
2024-08-02 11:47:51 +0800

    Update logo.tsx

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
         <strong className='text-2xl font-bold tracking-tighter xl:text-3xl'>
           wjt
         </strong>
-        <small className='font-semibold tracking-normal xl:text-xl'>.im</small>
+        <small className='font-semibold tracking-normal xl:text-xl'> .im</small>
       </Link>
     </h1>
   )
